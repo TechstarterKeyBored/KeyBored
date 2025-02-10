4fc885c4db6104fa9f1fe8f0ae83016e2fec0f8f
kyaz <nico.gross@edu.techstarter.de>
2025-02-10 13:54:51 +0100

    created database.js

---
+++ backend/auth.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcrypt");
+const db = require("./datenbank");
+
+const registerUser = (username, password, callback) =>{
+    bcrypt.hash(password, 10, (err, hash) =>{
+        if (err)  {
+            return callback(err);
+        }
+        db.run(
+            "INSERT INTO users (username, passwprd) VALUES (?, ?)",
+            [username, hash],
+            function (err) {
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, {id: this.lastID, username});
+            }
+        );
+    });
+};
+
+const loginUser = (username, password, callback) => {
+    db.get("SELECT * FROM users WHERE username = ?", [username], (err, user) => {
+        if (err) {
+            return callback(err);
+        }
+        if (!user) {
+            return callback(null, false);
+        }
+        bcrypt.compare(password, user.password, (err, res) => {
+            if (res) {
+                callback(null, user);
+            } else {
+                callback(null, false);
+            }
+        });
+    });
+};
+
+module.exports = {registerUser, loginUser};
---
+++ backend/datenbank.js
@@ -0,0 +1,29 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./game.db", (err) => {
+    if (err) {
+        console.error("Fehler beim Öffnen der Datenbank:", err.message);
+    } else{
+        console.log("Erfolgreich Verbunden.")
+    }
+});
+
+db.serialize(() =>{
+    db.run(
+        `CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )`
+);
+
+db.run(
+    `CREATE TABLE IF NOT EXISTS scores (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    users_id INTEGER,
+    score INTEGER NOT NULL,
+    FOREIGN KEY (user_ID) REFERENCES user(id)
+    )`
+);
+});
+module.exports = db;
