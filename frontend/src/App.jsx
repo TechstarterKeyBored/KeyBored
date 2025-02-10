d3df8c0b60acd3450046462e4aafff56122ab0dd
144droit <indieohren@gmail.com>
2025-02-10 11:30:54 +0100

    created frontend, backend and dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return(
+    <div className="App">
+      <header className="bg-gray-800 text-white p-4">
+        <h1>Test</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
