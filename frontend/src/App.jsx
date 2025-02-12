2ffd954996747ea8d7e8bd8079f4fe145529b44b
144droit <indieohren@gmail.com>
2025-02-12 09:54:29 +0100

    added react-player and test-url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
+import ReactPlayer from 'react-player'
 
 function App() {
   return(
     <div className="App">
       <header className="bg-gray-800 text-white p-4">
         <h1>Test</h1>
+        <ReactPlayer url="https://www.youtube.com/watch?v=l-iAS18rv68" />
       </header>
     </div>
   )
 }
 
-export default App
+export default App;
