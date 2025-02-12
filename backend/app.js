99916c211f9fbe2f0fa7ed8cc1cd9278eef3da50
144droit <indieohren@gmail.com>
2025-02-12 16:08:17 +0100

    added searchTrack function and /search endpoint

--- backend/app.js
+++ backend/app.js
@@ -12,12 +12,24 @@ app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
 });
 
+app.get('/search', async (req, res) => {
+    const {artist, title} = req.query;
+    const track_id = await searchTrack(artist, title);
+    res.json({ track_id });
+});
+
 app.get('/lyrics/:track_id', async (req, res) => {
     const track_id = Number(req.params.track_id);
     const lyrics = await getLyrics(track_id);
     res.json({ lyrics });
 });
 
+const searchTrack = async (artist, title) => {
+    const response = await fetch(`https://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&q_track=${title}&apikey=${API_KEY}`);
+    const data = await response.json();
+    console.log(data);
+    return data.message.body.track_list[0].track.commontrack_id;
+};
 
 const getLyrics = async (track_id) => {
     const response = await fetch(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?commontrack_id=${track_id}&apikey=${API_KEY}`);
