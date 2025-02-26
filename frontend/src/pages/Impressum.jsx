8db7576416479377637777bc5d1ebf017770dcc0
Ömer Akgül <oemer.akguel@gmx.de>
2025-02-26 15:24:58 +0100

    added Impressum.jsx

---
+++ frontend/src/pages/Impressum.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Impressum() {
+  return (
+    <div className="p-8 mx-auto w-4xl mt-5 text-white">
+      <h2 className="text-3xl font-bold mb-8 text-white">Impressum</h2>
+        <p className="text-white">
+              Angaben gemäß § 5 TMG: </p>
+        <p>Firma: Typing Trainer Projekt</p>
+        <p>Verantwortlich für
+              den Inhalt nach § 55 Abs. 2 </p>
+          <p>RStV: KeyBored Team </p>
+          <p>Adresse:Projekt Weg 500
+              15024 Berlin </p>
+          <p>E-Mail: keybored-team@tn.techstarter.de </p>
+          <p>Telefon:
+              + 49 170 1234 5678</p>
+          <h2 className="mt-5 text-2xl font-bold">Haftungsausschluss</h2>
+          <p className="mt-5"> Die Inhalte dieser Webseite
+        wurden mit größtmöglicher Sorgfalt erstellt. Wir übernehmen jedoch keine
+        Haftung für die Richtigkeit, Vollständigkeit und Aktualität der
+        bereitgestellten Informationen. Die Nutzung der Inhalte erfolgt auf
+        eigene Gefahr des Nutzers. Urheberrecht Die auf dieser Seite
+        veröffentlichten Inhalte unterliegen dem Urheberrecht. Eine
+        Vervielfältigung oder Verwendung von Texten, Bildern und anderen Medien
+        ist nur mit ausdrücklicher Zustimmung gestattet.
+      </p>
+    </div>
+  );
+}
+
+export default Impressum;
