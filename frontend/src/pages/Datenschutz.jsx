6c46984d9b01355ca33b2613f6cd2ace8212ab36
Ömer Akgül <oemer.akguel@gmx.de>
2025-02-26 15:24:18 +0100

    added Datenschutz.jsx

---
+++ frontend/src/pages/Datenschutz.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Datenschutz() {
+  return (
+    <div className="p-8 mx-auto w-4xl mt-5">
+      <h2 className="text-3xl font-bold mb-8 text-white">
+        Datenschutzerklärung
+      </h2>
+      <p className="text-white">
+        Wir nehmen den Schutz deiner persönlichen Daten ernst. Deine Daten
+        werden gemäß den gesetzlichen Bestimmungen des Datenschutzrechts
+        verarbeitet. Bei der Nutzung unseres Typing Trainers werden nur die für
+        die Nutzung erforderlichen Daten erfasst. Erhebung und Nutzung von Daten
+        Für die Registrierung und Anmeldung werden wir persönliche Daten wie
+        deinen Namen und deine E-Mail-Adresse erheben. Diese Daten werden
+        ausschließlich zur Bereitstellung des Dienstes verwendet. Deine Daten
+        werden niemals ohne deine Zustimmung an Dritte weitergegeben. Cookies
+        Unsere Webseite verwendet Cookies, um die Benutzererfahrung zu
+        verbessern. Du kannst die Verwendung von Cookies in den Einstellungen
+        deines Browsers anpassen. Deine Rechte Du hast das Recht, Auskunft über
+        die von uns gespeicherten Daten zu erhalten und diese zu korrigieren
+        oder zu löschen. Bei Fragen zum Datenschutz kannst du uns jederzeit
+        kontaktieren. Kontakt Für Fragen oder Anliegen zum Datenschutz kannst du
+        uns über die im Impressum angegebene E-Mail-Adresse erreichen.
+      </p>
+    </div>
+  );
+}
+
+export default Datenschutz;
