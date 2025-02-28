77b4e5015a45b4646b0495a27875b0ca1ebcb391
Ömer Akgül <oemer.akguel@gmx.de>
2025-02-28 12:41:48 +0100

    made some css changes

--- frontend/src/pages/Einfuhrung.jsx
+++ frontend/src/pages/Einfuhrung.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
 
 function Einführung() {
   return (
-        <div className="w-screen h-[65vh] overflow-hidden">
-          <video className="w-[130vh] h-[60vh] mx-auto mt-10 object-cover" controls>
-            <source src="/videos/Einführung.mp4" type="video/mp4" />
-            Dein Browser unterstützt das Video-Tag nicht.
-          </video>
-        </div>
+    <>
+      <h2 className="text-amber-100 text-3xl font-semibold text-center mt-13">
+        Einführung
+      </h2>
+      <p className="text-white p-4 text-md text-center mt-2 w-[100vh] mx-auto">
+        Lerne schnelles und präzises Tippen! 🚀✨ In diesem Video zeigen wir dir
+        effektive Übungen, um deine Tippgeschwindigkeit zu steigern. Leg los und
+        werde zum Tastatur-Profi!
+      </p>
+      <div className="w-screen h-[65vh] overflow-hidden">
+        <video
+          className="w-[100vh] h-[60vh] mx-auto mt-10 object-cover rounded-xl"
+          controls
+        >
+          <source src="/videos/Einführung.mp4" type="video/mp4" />
+          Dein Browser unterstützt das Video-Tag nicht.
+        </video>
+      </div>
+    </>
   );
 }
 
