085cb56119d73c55ef1d82683ebd0e43ccd31d7a
144droit <indieohren@gmail.com>
2025-03-05 16:24:52 +0100

    Testing commit I

--- frontend/tests/Header.test.jsx
+++ frontend/tests/Header.test.jsx
@@ -1,10 +1,9 @@
 import { describe, test, expect } from 'vitest';
-import Header from "../src/components/Header.jsx";
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
 test("Header renders correctly", () => {
 render(<header />);
-const Header = screen.getByRole("header");
+const Header = screen.getByRole("banner");
 expect(Header).toBeInTheDocument();
 });
