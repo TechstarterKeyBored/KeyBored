0897b92e8b80ab083864892faa1c63180c5ab7b7
144droit <indieohren@gmail.com>
2025-03-05 17:04:54 +0100

    added couple of tests

--- frontend/tests/Header.test.jsx
+++ frontend/tests/Header.test.jsx
@@ -1,9 +1,21 @@
-import { describe, test, expect } from 'vitest';
+import { test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 test("Header renders correctly", () => {
-render(<header />);
-const Header = screen.getByRole("banner");
-expect(Header).toBeInTheDocument();
+  render(<header />);
+  const Header = screen.getByRole("banner");
+  expect(Header).toBeInTheDocument();
+});
+
+test("h1 element contains the text 'KeyBored'", () => {
+  render(<h1>KeyBored</h1>);
+  const headingText = screen.getByText("KeyBored");
+  expect(headingText).toBeInTheDocument();
+});
+
+test("Header contains a navigation element", () => {
+  render(<nav />);
+  const navigation = screen.getByRole("navigation");
+  expect(navigation).toBeInTheDocument();
 });
